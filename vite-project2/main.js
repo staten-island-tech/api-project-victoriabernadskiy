55df87ac097a190ac795784c3d1c434d119a06a2
victoriabernadskiy <vbernadskiy0531@gmail.com>
2023-12-07 09:24:18 -0500

    api base

--- vite-project2/main.js
+++ vite-project2/main.js
@@ -1,2 +1,19 @@
-import './style.css'
-const apiEntry = "";
+import "./style.css";
+const URL = "https://api.nasa.gov/planetary/apod?api_key=NDeFF4wPtMyuheK8gYrnP8BJlJwZxlIG5mPsyshP";
+var APIKey = NDeFF4wPtMyuheK8gYrnP8BJlJwZxlIG5mPsyshP;
+async function getData(URL) {
+  try {
+    const response = await fetch(URL);
+    if (response.status != 200) {
+      throw new Error(response.statusText);
+    }
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+    document.querySelector("h1").textContent = data.content;
+  } catch (error) {}
+    document.querySelector("h1").textContent = "error";
+}
+
+getData(URL);
+
