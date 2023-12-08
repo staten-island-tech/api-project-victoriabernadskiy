928e011b2b368f3e37c9573060eac4240455a82f
victoriabernadskiy <vbernadskiy0531@gmail.com>
2023-12-08 09:24:14 -0500

    nasa

--- vite-project2/main.js
+++ vite-project2/main.js
@@ -1,5 +1,5 @@
-import "./style.css";
-const URL = "https://api.nasa.gov/planetary/apod?api_key=NDeFF4wPtMyuheK8gYrnP8BJlJwZxlIG5mPsyshP";
+// import "./style.css";
+/* const URL = "https://api.nasa.gov/planetary/apod?api_key=NDeFF4wPtMyuheK8gYrnP8BJlJwZxlIG5mPsyshP";
 var APIKey = NDeFF4wPtMyuheK8gYrnP8BJlJwZxlIG5mPsyshP;
 async function getData(URL) {
   try {
@@ -15,5 +15,30 @@ async function getData(URL) {
     document.querySelector("h1").textContent = "error";
 }
 
-getData(URL);
+getData(URL); */
 
+async function getData() {
+  let res = await fetch(
+    "https://api.nasa.gov/planetary/apod?api_key=NDeFF4wPtMyuheK8gYrnP8BJlJwZxlIG5mPsyshP"
+  );
+  let data = await res.json();
+  console.log(data);
+}
+getData();
+
+
+
+
+const url = 'https://api.nasa.gov/planetary/apod?api_key='
+const api_key = config.NASA_API_KEY
+
+const fetchNASAData = async () => {
+  try {
+    const response = await fetch(`${url}${api_key}`)
+    const data = await response.json()
+    console.log('NASA APOD data', data)
+    displayData(data)
+  } catch (error) {
+    console.log(error)
+  }
+}
