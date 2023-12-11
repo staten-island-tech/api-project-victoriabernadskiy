727e11947910d7515230bd947ac7686c050e00e1
victoriabernadskiy <vbernadskiy0531@gmail.com>
2023-12-11 09:23:32 -0500

    new apis

--- vite-project2/main.js
+++ vite-project2/main.js
@@ -1,50 +1,11 @@
-// import "./style.css";
-/* const URL = "https://api.nasa.gov/planetary/apod?api_key=NDeFF4wPtMyuheK8gYrnP8BJlJwZxlIG5mPsyshP";
-var APIKey = NDeFF4wPtMyuheK8gYrnP8BJlJwZxlIG5mPsyshP;
-async function getData(URL) {
-  try {
-    const response = await fetch(URL);
-    if (response.status != 200) {
-      throw new Error(response.statusText);
-    }
-    console.log(response);
-    const data = await response.json();
-    console.log(data);
-    document.querySelector("h1").textContent = data.content;
-  } catch (error) {}
-    document.querySelector("h1").textContent = "error";
+
+
+// NASA APOD
+const config = {
+  NASA_API_KEY: "NDeFF4wPtMyuheK8gYrnP8BJlJwZxlIG5mPsyshP"
 }
-
-getData(URL); */
-
-/* async function getData() {
-  let res = await fetch(
-    "https://api.nasa.gov/planetary/apod?api_key=NDeFF4wPtMyuheK8gYrnP8BJlJwZxlIG5mPsyshP"
-  );
-  let data = await res.json();
-  console.log(data);
-}
-getData();
-
-
-
-
 const url = 'https://api.nasa.gov/planetary/apod?api_key='
 const api_key = config.NASA_API_KEY
-
-const fetchNASAData = async () => {
-  try {
-    const response = await fetch(`${url}${api_key}`)
-    const data = await response.json()
-    console.log('NASA APOD data', data)
-    displayData(data)
-  } catch (error) {
-    console.log(error)
-  }
-} */
-
-const url = 'https://api.nasa.gov/planetary/apod?api_key='
-const api_key = config.NDeFF4wPtMyuheK8gYrnP8BJlJwZxlIG5mPsyshP
 
 const fetchNASAData = async () => {
   try {
@@ -57,3 +18,88 @@ const fetchNASAData = async () => {
 }
 
 fetchNASAData()
+
+
+// # of people in space rn
+const URL = `http://api.open-notify.org/astros.json`;
+async function getData(URL){
+try{
+const response1 = await fetch(URL);
+if(response1.status !=200) {
+    throw new Error(response1.statusText);
+}
+
+console.log(response1);
+const data = await response1.json();
+console.log('People in Space', data);
+document.querySelector("h1").textContent = data.content;
+document.querySelector("h1").textContent = data.author;
+} catch (error) {}
+document.querySelector("h1").textContent = "error";
+}
+
+getData(URL);
+
+
+// location of ISS rn
+const URL2 = `http://api.open-notify.org/iss-now.json`;
+async function getData(URL2){
+try{
+const response2 = await fetch(URL2);
+if(response2.status !=200) {
+    throw new Error(response2.statusText);
+}
+
+console.log(response);
+const data = await response2.json();
+console.log(data);
+document.querySelector("h1").textContent = data.content;
+document.querySelector("h1").textContent = data.author;
+} catch (error) {}
+document.querySelector("h1").textContent = "error";
+}
+
+getData(URL2);
+
+
+//ISS api another (easier to read)
+
+const URL3 = `https://api.wheretheiss.at/v1/satellites/25544`;
+async function getData(URL3){
+try{
+const response3 = await fetch(URL3);
+if(response3.status !=200) {
+    throw new Error(response3.statusText);
+}
+
+console.log(response3);
+const data = await response3.json();
+console.log(data);
+document.querySelector("h1").textContent = data.content;
+document.querySelector("h1").textContent = data.author;
+} catch (error) {}
+document.querySelector("h1").textContent = "error";
+}
+
+getData(URL3);
+
+//smth cool maybe
+
+const URL4 = `https://api.le-systeme-solaire.net/rest/bodies/`;
+async function getData(URL4){
+try{
+const response4 = await fetch(URL4);
+if(response.status !=200) {
+    throw new Error(response.statusText);
+}
+
+console.log(response4);
+const data = await response4.json();
+console.log(data);
+document.querySelector("h1").textContent = data.content;
+document.querySelector("h1").textContent = data.author;
+} catch (error) {}
+document.querySelector("h1").textContent = "error";
+}
+
+getData(URL4);
